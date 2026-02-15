145ced1d302a2ddeeacba29fcbd207ba3794c78f
semonbenyamen <semobenyamen@gmail.com>
2026-02-15 13:24:50 +0200

    task1

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
+app.use(express.json());
 const mongoose = require("mongoose");
 
+const Task = require("./models/Task");
 
-const User = require("./models/User");
-
-const app = express();
-const bcrypt = require("bcrypt");
-const Product = require("./models/Product");
-app.use(express.json());
-
-const mongo_url = process.env.DB_URL;
 async function dbconnection() {
     try {
-        await mongoose.connect(mongo_url);
+        await mongoose.connect("mongodb://127.0.0.1:27017/firstApp");
         console.log("MongoDB connected successfully");
     } catch (err) {
         console.error("MongoDB connection error:", err);
